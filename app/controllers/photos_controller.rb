41340bd7ed177aeeb8479e6c235fef149dd91c68
Isay Lucas <ilucash25@gmail.com>
2022-05-20 12:11:13 -0500

    scaffold

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,43 @@
 class PhotosController < ApplicationController
+  def index
+    @photos = Photo.all
+  end
+
+  def new
+  end
+
+  def show
+    @photo = Photo.find params[:id]
+  end
+
+  def edit
+    @id = params[:id]
+  end
+
+  def update
+    photo = Photo.find(params[:id])
+    photo.image_url = params[:photo][:image_url]
+
+    photo.save
+    redirect_to photo
+  end
+
+  # PUT/PATCH /photos/:id
+  def create
+    photo = Photo.new
+    photo.title = params[:title]
+    photo.image_url = params[:image_url]
+
+    photo.save
+
+    redirect_to "/photos"
+  end
+
+  # DELETE /photos/:id
+  def destroy
+    photo = Photo.find(params[:id])
+    photo.destroy
+
+    redirect_to "/photos"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   resources :tweets
+  resources :photos
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "main#hello"
   get "/hola", to: "main#hello"
   
   get "/saludo", to: "main#hi"
   post "hi", to:"main#greetings"
+
+
 end
